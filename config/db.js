27507a27a332a226f0e485db46fde016b9a99025
Diab16 <abdohdiab166@gmail.com>
2023-10-12 00:08:37 +0300

    delete old connection comment

--- config/db.js
+++ config/db.js
@@ -9,15 +9,7 @@ async function connectToDB() {
     });
     console.log("Database is running...");
   } catch (error) {
-    console.log("Connection faild to MongoDB!", error);
+    console.log("Connection failed to MongoDB!", error);
   }
 }
 module.exports = connectToDB;
-
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Database is running..."))
-//   .catch((error) => console.log("Connection faild to MongoDB!", error));
